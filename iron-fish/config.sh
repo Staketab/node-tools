a6e3f6a7dc39b1bba5b61138fdf6daea06d02e21
Ducca <contact@icohigh.net>
2021-07-08 13:40:05 +0300

    ironfish config system install

---
+++ iron-fish/config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+YELLOW="\033[33m"
+GREEN="\033[32m"
+NORMAL="\033[0m"
+
+curl -s https://raw.githubusercontent.com/Staketab/node-tools/main/components/install.sh | bash
+
+echo -e "$YELLOW Components updated.$NORMAL"
+echo "---------------"
+
+echo "TAG=ghcr.io/iron-fish/ironfish:latest" > $HOME/.env
+
+echo -e "$YELLOW ENV for docker-compose created.$NORMAL"
+echo "---------------"
+
+echo 'export RUN="docker run --rm --tty --interactive --net=host -v $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest"' >> ~/.profile
+source ~/.profile
+
+echo -e "$GREEN ALL settings and configs created.$NORMAL"
+echo "---------------"
