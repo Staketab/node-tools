513864b95983c95de36d51d69a8c0807c0da51da
Ducca <contact@icohigh.net>
2021-07-10 10:41:38 +0300

    test config

--- iron-fish/config.sh
+++ iron-fish/config.sh
@@ -3,10 +3,19 @@
 YELLOW="\033[33m"
 GREEN="\033[32m"
 NORMAL="\033[0m"
+THREADS=$1
+DIR=$2
+PORT=$3
+
+set -u
 
 curl -s https://raw.githubusercontent.com/Staketab/node-tools/main/components/install.sh | bash
 
-echo "TAG=ghcr.io/iron-fish/ironfish:latest" > $HOME/.env
+echo "TAG=ghcr.io/iron-fish/ironfish:latest
+THREAD='--threads ${THREADS}'
+DATA='--datadir=~/.${DIR}/'
+PORT='--port=${PORT}'
+" > $HOME/.env
 
 echo -e "$YELLOW ENV for docker-compose created.$NORMAL"
 echo "---------------"
