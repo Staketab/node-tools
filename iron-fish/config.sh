3120da79c48c42e3615507a836406467ee678b53
Ducca <contact@icohigh.net>
2021-08-04 11:40:11 +0300

    ironfish config update
    added config.json, keys folder, new env

--- iron-fish/config.sh
+++ iron-fish/config.sh
@@ -3,12 +3,23 @@
 YELLOW="\033[33m"
 GREEN="\033[32m"
 NORMAL="\033[0m"
-THREADS=$1
-PORT=$2
+NODENAME=$1
+GRAFFITI=$2
+THREADS=$3
+PORT=$4
 
-set -u
-
-curl -s https://raw.githubusercontent.com/Staketab/node-tools/main/components/install.sh | bash
+if [ "$NODENAME" == "" ]; then
+    echo -e "$YELLOW none NODENAME ENV.$NORMAL"
+fi
+if [ "$GRAFFITI" == "" ]; then
+    echo -e "$YELLOW none GRAFFITI ENV.$NORMAL"
+fi
+if [ "$THREADS" == "" ]; then
+    echo -e "$YELLOW none THREADS ENV.$NORMAL"
+fi
+if [ "$PORT" == "" ]; then
+    echo -e "$YELLOW none PORT ENV.$NORMAL"
+fi
 
 sudo /bin/bash -c  'echo "TAG=ghcr.io/iron-fish/ironfish:latest
 THREAD='${THREADS}'
@@ -18,8 +29,23 @@ PORT='${PORT}'
 echo -e "$YELLOW ENV for docker-compose created.$NORMAL"
 echo "---------------"
 
+mkdir -p $HOME/.ironfish/keys
+sudo /bin/bash -c  'echo "{
+    \"bootstrapNodes\": [
+        \"test.bn1.ironfish.network\"
+    ],
+    \"blockGraffiti\": \"'${GRAFFITI}'\",
+    \"nodeName\": \"'${NODENAME}'\",
+    \"enableSyncing\": \"true\",
+    \"enableTelemetry\": \"true\"
+}
+" > $HOME/.ironfish/config.json'
+
+echo -e "$YELLOW Config created.$NORMAL"
+echo "---------------"
+
 echo 'export RUN="docker run --rm --tty --interactive --net=host -v $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest"' >> $HOME/.profile
-source $HOME/.profile
+. $HOME/.profile
 
 echo -e "$GREEN ALL settings and configs created.$NORMAL"
 echo "---------------"
