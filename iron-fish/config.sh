c20ee8df28d155078cec9f3ce08ab4258e74225a
Ducca <contact@icohigh.net>
2021-08-06 14:00:42 +0300

    ironfish config updated

--- iron-fish/config.sh
+++ iron-fish/config.sh
@@ -3,31 +3,68 @@
 YELLOW="\033[33m"
 GREEN="\033[32m"
 NORMAL="\033[0m"
-NODENAME=$1
-GRAFFITI=$2
-THREADS=$3
-PORT=$4
 
-if [ "$NODENAME" == "" ]; then
-    echo -e "$YELLOW none NODENAME ENV.$NORMAL"
-fi
-if [ "$GRAFFITI" == "" ]; then
-    echo -e "$YELLOW none GRAFFITI ENV.$NORMAL"
-fi
-if [ "$THREADS" == "" ]; then
-    echo -e "$YELLOW none THREADS ENV.$NORMAL"
-fi
-if [ "$PORT" == "" ]; then
-    echo -e "$YELLOW none PORT ENV.$NORMAL"
-fi
+function setup {
+  nodename "${1}"
+  graffiti "${2}"
+  threads "${3}"
+  port "${4}"
+}
 
+function nodename {
+  NODENAME=${1}
+}
+
+function graffiti {
+  GRAFFITI=${1}
+}
+
+function threads {
+  THREADS=--threads=${1:-"1"}
+}
+
+function port {
+  PORT=--port=${1:-"9033"}
+}
+
+function envFile {
 sudo /bin/bash -c  'echo "TAG=ghcr.io/iron-fish/ironfish:latest
 THREAD='${THREADS}'
 PORT='${PORT}'
 " > $HOME/.env'
+}
 
-echo -e "$YELLOW ENV for docker-compose created.$NORMAL"
-echo "---------------"
+function profiles {
+echo 'export RUN="docker run --rm --tty --interactive --net=host -v $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest"' >> $HOME/.profile
+. $HOME/.profile
+}
+
+function installation {
+setup "${1}" "${2}" "${3}" "${4}"
+ENV="$HOME/.env"
+if [ -f "$ENV" ]; then
+    echo -e "$YELLOW Found an ironfish ENV for docker-compose. Choose an option:$NORMAL"
+    echo -e "$RED 1$NORMAL -$YELLOW Reinstall ironfish config.$NORMAL"
+    echo -e "$RED 2$NORMAL -$YELLOW Do nothing.$NORMAL"
+    read -p "Your answer: " ANSWER
+    if [ "$ANSWER" == "1" ]; then
+        rm -rf $ENV
+        envFile
+    elif [ "$ANSWER" == "2" ]; then
+        echo -e "$YELLOW The option to do nothing is selected. Continue...$NORMAL"
+    fi
+else
+    envFile
+    echo -e "$GREEN Ironfish ENV for docker-compose created.$NORMAL"
+fi
+
+EXP='export RUN="docker run --rm --tty --interactive --net=host -v $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest"'
+if grep -R "$EXP" $HOME/.profile; then
+    echo -e "$GREEN Ironfish ENV found.$NORMAL"
+else
+    profiles
+    echo -e "$GREEN Ironfish ENV installed.$NORMAL"
+fi
 
 mkdir -p $HOME/.ironfish/keys
 sudo /bin/bash -c  'echo "{
@@ -44,8 +81,26 @@ sudo /bin/bash -c  'echo "{
 echo -e "$YELLOW Config created.$NORMAL"
 echo "---------------"
 
-echo 'export RUN="docker run --rm --tty --interactive --net=host -v $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest"' >> $HOME/.profile
-. $HOME/.profile
-
 echo -e "$GREEN ALL settings and configs created.$NORMAL"
 echo "---------------"
+}
+
+while getopts ":n:g:t:p:" o; do
+  case "${o}" in
+    n)
+      n=${OPTARG}
+      ;;
+    g)
+      g=${OPTARG}
+      ;;
+    t)
+      t=${OPTARG}
+      ;;
+    p)
+      p=${OPTARG}
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+installation "${n}" "${g}" "${t}" "${p}"
