46a0a17829cf90ad718713f99777d5547b986d25
Ducca <contact@icohigh.net>
2021-09-04 18:06:49 +0300

    new user script setup update

--- components/user/new.sh
+++ components/user/new.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-read -p "Enter USERNAME": USER
+function setup {
+  username "${1}"
+}
 
+function username {
+  USER=${1}
+}
+
+function install {
+setup "${1}"
 adduser ${USER}
 usermod -aG sudo ${USER}
 echo "${USER} ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 echo "DenyUsers ${USER}" | sudo tee -a /etc/ssh/sshd_config
 sudo service ssh restart
 su - ${USER}
+}
+
+while getopts ":u:" o; do
+  case "${o}" in
+    u)
+      u=${OPTARG}
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+install "${u}"
