055b79710befe01afc0fcf7f794e10f13c0aff0e
Ducca <contact@icohigh.net>
2021-09-04 17:54:19 +0300

    Update install.sh

--- components/install.sh
+++ components/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 GREEN="\033[32m"
+NORMAL="\033[0m"
 
 sudo apt update && sudo apt upgrade -y \
 && sudo apt install docker.io curl -y \
@@ -11,6 +12,6 @@ sudo apt update && sudo apt upgrade -y \
 && sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python libssl-dev git cmake make perl tmux ufw gcc unzip zip jq golang-statik -y
 
 
-echo "---------------"
-echo -e "$GREEN ALL COMPONENTS INSTALLED.\033[0m"
-echo "---------------"
+echo "-------------------------------------------------------------------"
+echo -e "$GREEN ALL COMPONENTS INSTALLED.$NORMAL"
+echo "-------------------------------------------------------------------"
