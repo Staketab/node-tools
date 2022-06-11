59096cbd4f9c2060cc9ac55a94af001de4be504c
Ducca <64695363+icohigh@users.noreply.github.com>
2022-06-11 18:09:12 +0300

    add custom docker-compose version

--- components/docker/install.sh
+++ components/docker/install.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+curl -s https://raw.githubusercontent.com/Staketab/node-tools/main/logo.sh | bash
+
+set -e
+
 GREEN="\033[32m"
 NORMAL="\033[0m"
+VERSION=$1
 
-sudo apt update \
+if [ "$VERSION" == "" ]; then
+    VERSION="1.29.2"
+fi
+
+export VERSION="$VERSION" \
+&& sudo apt update \
 && sudo apt install docker.io curl -y \
 && sudo systemctl start docker \
 && sudo systemctl enable docker \
-&& sudo curl -L https://github.com/docker/compose/releases/download/1.29.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+&& sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
 && sudo chmod +x /usr/local/bin/docker-compose
 
 echo "-------------------------------------------------------------------"
-echo -e "$GREEN DOCKER INSTALLED.$NORMAL"
+echo -e "$GREEN DOCKER v$VERSION INSTALLED.$NORMAL"
 echo "-------------------------------------------------------------------"
