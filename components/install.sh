4d4c740f535e5feab6b9ba025bfd3ecc3335405b
Ducca <64695363+icohigh@users.noreply.github.com>
2023-03-25 11:17:41 +0300

    fix full components link

--- components/install.sh
+++ components/install.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade -y \
 && sudo apt install docker.io curl -y \
 && sudo systemctl start docker \
 && sudo systemctl enable docker \
-&& sudo curl -L https://github.com/docker/compose/releases/download/1.29.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+&& sudo wget -O /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/v2.17.0/docker-compose-`uname -s`-`uname -m` \
 && sudo chmod +x /usr/local/bin/docker-compose \
 && sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python libssl-dev git cmake make perl tmux ufw gcc unzip zip jq golang-statik -y
 
